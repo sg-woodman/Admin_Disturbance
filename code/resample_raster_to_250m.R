85b221d8bd8e4881d448006a9d1dcd3fb6a4df19
Samuel Woodman <samuel.woodman@gmail.com>
2021-07-23 11:06:42 -0600

    init

---
+++ code/resample_raster_to_250m.R
@@ -0,0 +1,31 @@
+# Title: Downscale C-flux data
+# Author details: Author: Sam Woodman 
+# Contact details: samuel.woodman@gmail.com
+# Description: Reduce resolution of 30m C-flux raster to 250m 
+
+## Analyses on 30m rasters for the entire study area results in over 2 billion
+## pixels. This makes any subsequebt analyses slow and impractical. Therefore, 
+## downsampling to 250m is more appropriate. 250m was selected as this was the 
+## resolution of the belowground C data. 
+
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse, warn.conflicts = F)
+library(here)
+library(terra)
+
+# Load data ---------------------------------------------------------------
+
+cflux_30 <- rast(here("data/raw/aou_cflux_30.tif"))
+r_250 <- rast(here("data/raw/raster_250m_template.tif")) # originally from MODIS pheno data
+
+# Resample raster ---------------------------------------------------------
+
+cflux_250 <- terra::resample(cflux_30, r_250, method = "bilinear")
+
+# Save output -------------------------------------------------------------
+
+writeRaster(cflux_250, here("data/processed/aou_clux_250.tif"))
+

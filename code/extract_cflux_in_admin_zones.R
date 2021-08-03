6de361c2dd51c784dc0bb9958dd7e95eb3515d95
Samuel Woodman <samuel.woodman@gmail.com>
2021-08-03 09:26:16 -0600

    update to per hectacre cflux data from per pixel

--- code/extract_cflux_in_admin_zones.R
+++ code/extract_cflux_in_admin_zones.R
@@ -6,7 +6,10 @@
 ## To summarize the flux of C from forest in Ontario we need to select the 
 ## appropriate spatial scale for the analysis. Here, we have selected 
 ## administrative zones available GeoHub. Using exactextractr we can calculate 
-## the mean, median, sum, etc. of C-flux in each catchment. 
+## the mean, median, sum, etc. of C-flux in each catchment. The Hansen C-flux 
+## data comes in two forms: per pixel and per hectacre. First attempts used per 
+## pixel and calculated per ha but once this approach was shown to be useful the
+## per ha data was downloaded and extracted. 
 
 
 # Packages ----------------------------------------------------------------
@@ -19,7 +22,7 @@ library(exactextractr)
 
 # Load data ---------------------------------------------------------------
 
-cflux_30 <- raster(here("data/raw/aou_cflux_30.tif"))
+cflux_ha_30 <- raster(here("data/raw/aou_cflux_per_ha_30.tif"))
 admin_zone_nolakes <- 
   # generated and validated using QGIS (faster than R in this case)
   st_read(here("data/processed/admin_zone_lakes-removed_valid.gpkg")) %>% 
@@ -28,14 +31,15 @@ admin_zone_nolakes <-
 
 # Extract  ----------------------------------------------------------------
 
-cflux_admin <- exact_extract(cflux_30, 
+## per ha extraction
+cflux_admin_ha <- exact_extract(cflux_ha_30, 
                              admin_zone_nolakes,
                              c("mean", "sum", "median", "variance", "count"))
 
-colnames(cflux_admin) <- paste("cflux", colnames(cflux_admin), sep="_")
+colnames(cflux_admin_ha) <- paste("cflux", colnames(cflux_admin_ha), sep="_")
 
-cflux_admin_df <- admin_zone_nolakes %>%
+cflux_admin_ha_df <- admin_zone_nolakes %>%
   st_set_geometry(NULL) %>%
-  bind_cols(., cflux_admin)
+  bind_cols(., cflux_admin_ha)
 
-write_csv(cflux_admin_df, here("data/processed/cflux_admin_extract.csv"))
+write_csv(cflux_admin_ha_df, here("data/processed/cflux_admin_ha_extract.csv"))

16eb48ea4be46a1022a5288eca50af51e6fae280
Samuel Woodman <samuel.woodman@gmail.com>
2021-08-30 14:28:44 -0600

    fix extent and mask by fri data

--- code/rasterize_dist_polygons.R
+++ code/rasterize_dist_polygons.R
@@ -18,16 +18,17 @@ library(terra)
 # Load data ---------------------------------------------------------------
 
 aou <- vect(here("data/raw/AoU.gpkg"))
-cflux_250 <- rast(here("data/processed/aou_cflux_250m.tif"))
-aou_rast <- rast(here("data/processed/aou_rast_250m.tif"))
-aou_lakes_rast <- rast( here("data/processed/aou_lakes_rast_250m.tif"))
+cflux_30 <- rast(here("data/raw/aou_cflux_per_ha_30.tif"))
+aou_rast <- rast(here("data/processed/aou_rast_30.tif"))
+aou_lakes_rast <- rast(here("data/processed/aou_lakes_rast_30m.tif"))
 all_dist <- vect(here("data/processed/all_dist_2001-2019.gpkg"))
+fri_age_aou <- rast(here("data/processed/fri_age_aou.tif"))
 
 # Rasterize disturbance polygons ------------------------------------------
 
 ## Taking all disturbance events from 2001 to 2019, sum the number of events
 ## that occurred in a given pixel. 
-all_dist_raster <- rasterize(all_dist, cflux_250, "count", 
+all_dist_raster <- rasterize(all_dist, cflux_30, "count", 
                              background = 0, sum = T)
 ## Clean rasterized disturbance totals by croping, removing lakes, and 
 ## pixels that fall outside AoU
@@ -36,7 +37,9 @@ dist_rast <- all_dist_raster %>%
   # mask lakes 
   terra::mask(., aou_lakes_rast) %>% 
   # mask values outside aou
-  terra::mask(., aou_rast)
+  terra::mask(., aou_rast) %>% 
+  # mask to only vpixels with fri data
+  terra::mask(., fri_age_aou)
 plot(dist_rast)
 
 # Save output -------------------------------------------------------------

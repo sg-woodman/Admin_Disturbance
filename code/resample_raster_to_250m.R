f73c053a8c0b9a5deb00341dde4758ed4ac49e12
Samuel Woodman <samuel.woodman@gmail.com>
2022-06-23 11:59:40 -0600

    add additional datasets

--- code/resample_raster_to_250m.R
+++ code/resample_raster_to_250m.R
@@ -28,6 +28,19 @@ cflux_30_ha_3161 <- rast(here("data/processed/aou_cflux_30m_ha_3161.tif"))
 
 land_cover_3161 <- rast("/Volumes/Backup/Large_Files/ON_Land_Cover.tif")
 
+mean_t_3161 <- rast(here("data/raw/mean_t_01_19.tif"))
+
+human_footprint_3161 <- rast(here("data/raw/ontario_human_footprint.tif"))
+
+harvest_3161 <- rast(here("data/raw/harvest_3161.tif"))
+
+evi_3161 <- rast(here("data/raw/mean_evi_3161.tif"))
+
+evi_sen_3161 <- rast(here("data/raw/evi_sen_slope_3161.tif"))
+
+fri_overstory_3161 <- rast(here("data/processed/fri_overstory_3161.tif"))
+
+fri_understory_3161 <- rast(here("data/processed/fri_understory_3161.tif"))
 
 
 # Create raster template --------------------------------------------------
@@ -41,7 +54,15 @@ r_250_3161 <- rast(res = c(250,250),
 
 writeRaster(r_250_3161, here("data/raw/raster_250_tamplate_3161.tif"))
 
+r_500_3161 <- rast(res = c(500,500),
+                   extent = ext(cflux_30_ha_3161),
+                   crs = epsg_3161, 
+                   vals = 1)
+
+writeRaster(r_500_3161, here("data/raw/raster_500_tamplate_3161.tif"))
+
 # Resample raster ---------------------------------------------------------
+
 
 cflux_250 <- terra::resample(cflux_30, r_250, method = "bilinear")
 
@@ -51,6 +72,7 @@ cflux_250_ha <- terra::resample(cflux_30_ha, r_250, method = "bilinear")
 
 cflux_250_ha <- terra::crop(cflux_250_ha, aou)
 
+## 3161; 250m
 cflux_250_ha_3161 <- terra::resample(cflux_30_ha_3161, r_250_3161, 
                                      method = "bilinear")
 
@@ -60,6 +82,52 @@ land_cover_250_3161 <- terra::resample(land_cover_3161, r_250_3161,
                                      method = "near")
 
 land_cover_250_3161 <- terra::crop(land_cover_250_3161, aou_3161)
+
+## 3161; 500m
+cflux_500_ha_3161 <- terra::resample(cflux_30_ha_3161, r_500_3161, 
+                                     method = "bilinear")
+
+cflux_500_ha_3161 <- terra::crop(cflux_500_ha_3161, aou_3161)
+##
+land_cover_500_3161 <- terra::resample(land_cover_3161, r_500_3161, 
+                                       method = "near")
+
+land_cover_500_3161 <- terra::crop(land_cover_500_3161, aou_3161)
+##
+mean_t_500_3161 <- terra::resample(mean_t_3161, r_500_3161, 
+                                       method = "bilinear")
+
+mean_t_500_3161 <- terra::crop(mean_t_500_3161, aou_3161)
+##
+human_footprint_500_3161 <- terra::resample(human_footprint_3161, r_500_3161, 
+                                   method = "bilinear")
+
+human_footprint_500_3161 <- terra::crop(human_footprint_500_3161, aou_3161)
+##
+harvest_500_3161 <- terra::resample(harvest_3161, r_500_3161, 
+                                            method = "near")
+
+harvest_500_3161 <- terra::crop(harvest_500_3161, aou_3161)
+##
+evi_500_3161 <- terra::resample(evi_3161, r_500_3161, 
+                                    method = "bilinear")
+
+evi_500_3161 <- terra::crop(evi_500_3161, aou_3161)
+##
+evi_sen_500_3161 <- terra::resample(evi_sen_3161, r_500_3161, 
+                                method = "bilinear")
+
+evi_sen_500_3161 <- terra::crop(evi_sen_500_3161, aou_3161)
+##
+overstory_500_3161 <- terra::resample(fri_overstory_3161, r_500_3161, 
+                                    method = "bilinear")
+
+overstory_500_3161 <- terra::crop(overstory_500_3161, aou_3161)
+##
+understory_500_3161 <- terra::resample(fri_understory_3161, r_500_3161, 
+                                      method = "bilinear")
+
+understory_500_3161 <- terra::crop(understory_500_3161, aou_3161)
 # Save output -------------------------------------------------------------
 
 writeRaster(cflux_250, here("data/processed/aou_cflux_250m.tif"), overwrite = T)
@@ -69,3 +137,47 @@ writeRaster(cflux_250_ha, here("data/processed/aou_cflux_250m_ha.tif"), overwrit
 writeRaster(cflux_250_ha_3161, here("data/processed/aou_cflux_250m_ha_3161.tif"), overwrite = T)
 
 writeRaster(land_cover_250_3161, here("data/processed/land_cover_250_3161.tif"), overwrite = T)
+
+writeRaster(cflux_500_ha_3161, here("data/processed/aou_cflux_500m_ha_3161.tif"), overwrite = T)
+
+writeRaster(land_cover_500_3161, here("data/processed/land_cover_500_3161.tif"), overwrite = T)
+
+writeRaster(mean_t_500_3161, here("data/processed/mean_t_500_3161.tif"), overwrite = T)
+
+writeRaster(human_footprint_500_3161, here("data/processed/human_footprint_500_3161.tif"), overwrite = T)
+
+writeRaster(harvest_500_3161, here("data/processed/harvest_500_3161.tif"), overwrite = T)
+
+writeRaster(evi_500_3161, here("data/processed/evi_500_3161.tif"), overwrite = T)
+
+writeRaster(evi_sen_500_3161, here("data/processed/evi_sen_500_3161.tif"), overwrite = T)
+
+writeRaster(overstory_500_3161, here("data/processed/overstory_500_3161.tif"), overwrite = T)
+
+writeRaster(understory_500_3161, here("data/processed/understory_500_3161.tif"), overwrite = T)
+
+
+
+## Resample harvest tp 30
+
+cflux_30_ha_3161 <- rast(here("data/processed/aou_cflux_30m_ha_3161.tif"))
+
+
+harvest_3161 <- rast(here("data/raw/harvest_3161.tif"))
+
+harvest_30_3161 <- terra::resample(harvest_3161, cflux_30_ha_3161, method = "near")
+
+harvest_30_3161[harvest_30_3161 > 0] <- NA
+
+aou_3161 <- vect(here("data/processed/"))
+
+harvest_30_3161_aou <- harvest_30_3161 %>% 
+  terra::crop(., aou_3161) %>%
+  # mask lakes 
+  terra::mask(., aou_lakes_rast_3161) %>% 
+  # mask values outside aou
+  terra::mask(., aou_rast_3161)
+plot(harvest_30_3161)
+
+writeRaster(harvest_30_3161, here("data/processed/harvest_mask_30_3161.tif"))
+
